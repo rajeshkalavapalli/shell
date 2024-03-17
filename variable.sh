8e51a72ab8eb6f2d92a65fb2baa6e8a4833acc0d
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-17 08:48:03 +0530

    sample just modifed

--- variable.sh
+++ variable.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
-Person1=tony
-person2=shiny
-echo "$person1 :hi $person2 good morning "
-echo "$person2:hi $person1 very good morning"
-echo "$person1 :how are you $person2?"
-echo "$person2:iam doing great ,thanks for asking"
+Person1=Tony
+person2=Shiny
+
+echo "$person1: hi $person2 good morning "
+echo "$person2: hi $person1 very good morning"
+echo "$person1: how are you $person2?"
+echo "$person2: iam doing great ,thanks for asking"
