ed811fb90629763086c3f7700effb3947a3985c7
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-17 12:36:30 +0530

    arguments in  variable

--- 03.variablearg.sh
+++ 03.variablearg.sh
@@ -1,7 +1,9 @@
 Person1=$1
 Person2=$2
+Person3=$3
 
 echo "$Person1: hi $Person2 good morning "
 echo "$Person2: hi $Person1 very good morning"
 echo "$Person1: how are you $Person2?"
 echo "$Person2: iam doing great ,thanks for asking"
+echo "$Person3: "how are you $Person1,$Person2....?"
