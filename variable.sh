3e9929eb4b26592735a31f8264f74ccf6751af2a
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-17 08:32:05 +0530

    sample just modifed

--- variable.sh
+++ variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
 echo "Rajesh :hi kishore good morning "
-echo "kishore :hi rajesh very good morning"
-echo "how are you kishore?"
-echo "iam doing great ,thanks for asking"
+echo "kishore:hi rajesh very good morning"
+echo "Rajesh :how are you kishore?"
+echo "kishore:iam doing great ,thanks for asking"
