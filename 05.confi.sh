72baa2e582908eb6c2dca1a5562f80e5eb3ead7b
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-19 20:43:00 +0530

    user input not vissible

--- 05.confi.sh
+++ 05.confi.sh
@@ -4,8 +4,9 @@ echo "please enter your username"
 
 read -s USERNAME 
 
-echo "your user name is: $USERNAME"
+
 
 echo "please enter you password "
 read -s Password 
+echo "your user name is: $USERNAME"
 echo "your password is :$Password"
