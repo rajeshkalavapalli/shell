95e970693971bd452eb2a221ed665dde23d83c63
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-20 08:24:06 +0530

    corrected  condition

--- 07.condition.sh
+++ 07.condition.sh
@@ -2,9 +2,9 @@
 
 Number=$1
 
-if ($number -gt 100) then  
-echo "given $number is gretaer then 100"
-
-else 
-echo "given $number is not grater than 100"
-fi 
+if [ $number -gt 100 ]
+then
+   echo "given $number is gretaer then 100"
+else
+   echo "given $number is not grater than 100"
+fi
