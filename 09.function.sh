445ceefd61357dcd6a1f5414980f1c1302ea2797
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-20 12:22:55 +0530

    color added

--- 09.function.sh
+++ 09.function.sh
@@ -3,15 +3,19 @@
 ID=$(id -u)
 #echo "scriptname:$0"
 TIMESTAMP=$(date +%F-%H-%M-%S) 
+
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+
 VALIDATE(){
     if [ $1 -ne 0 ]
      then 
-     echo -e "Error::$2 ...$R faild $N"
+     echo -e "Error::$2 ... $R faild $N"
      exit 1
     else 
      echo -e "$2 .......$G sucess $N"
