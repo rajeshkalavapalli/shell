2315f4e8a218c6f53395506dbb81004a961fdaca
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-20 22:57:57 +0530

    clour    added

--- logs.sh
+++ logs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 ID=$(id -u)
 
-R=\e[31m
-G=\e[32m
-Y=\e[33m
-N=\e[0m
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 echo "script name :$0"
 
@@ -15,7 +15,7 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 .......${R} faild ${N}"
+        echo -e "$2 .......${R} failed ${N}"
         exit 1
     else 
         echo -e " $2 ........${G} succes ${N}"
