61100348c4818fc1324e2408d43220bf098604cf
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-20 09:16:27 +0530

    install mysql vallidation

--- 08.installsql.sh
+++ 08.installsql.sh
@@ -2,15 +2,21 @@
 
 ID=$(id -u)
 
-if [ $ID -eq 0 ]
+if [ $ID -ne 0 ]
 then 
-echo "your a root user"
+echo "your not a root user"
 else  
-echo "your not a roote user" 
-
+echo "your a roote user" 
+exit 1 
 fi
 
 yum update -y 
 
 yum install -y mysql 
 
+if [ $? -ne 0 ]
+then 
+else 
+echo "Error:: not succes "
+else 
+echo " installation sucess"
