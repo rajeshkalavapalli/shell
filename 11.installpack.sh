284783c908a2cb32557b6eabd70f0075f866b5b6
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-20 15:00:50 +0530

    package install

--- 11.installpack.sh
+++ 11.installpack.sh
@@ -3,11 +3,10 @@ ID=$(id -u)
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
-if [$id -ne 0]
+if [ $id -ne 0 ]
    then 
    echo "error :: your not a route user"
    else 
    echo "your a route user"
    fi
 echo "all argument passed :$@"
-   
