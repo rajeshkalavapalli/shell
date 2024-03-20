f8184e6810a9b580d7e3938f3f85f273ae143aaa
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-20 14:59:25 +0530

    package install

---
+++ 11.installpack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+if [$id -ne 0]
+   then 
+   echo "error :: your not a route user"
+   else 
+   echo "your a route user"
+   fi
+echo "all argument passed :$@"
+   
