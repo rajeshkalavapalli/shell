55e2fff8e7a43bb7af5dc23be0847e87edc8a83f
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-20 22:09:08 +0530

    script name

---
+++ logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+ID=$(id -u)
+
+echo "script name :$0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 .......faild"
+        exit 1
+    else 
+        echo " $2 ........succes"
+    fi   
+
+}
+
+if [ $ID -ne 0 ]
+  then 
+    echo "Error :: please run this  script with root user" 
+    exit 1
+  else
+    echo "you're a root user"
+fi  
+
+yum install mysql -y
+
+VALIDATE $? "installed mysql"
+
+yum install git  -y 
+
+VALIDATE $? "installed git"
