fae4b162ab5a4e1f36ecf00769f2d398426d7b39
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-27 15:30:37 +0530

    conversation new

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash 
 
-echo "ramesh: how are you" 
-echo "vinny: iam good ramesh"
-echo "ramesh: how are you doing today"
-echo "viny : fine thanks" 
+PERSON1=Ramesh 
+PERSON2=vinny 
+
+echo "$PERSON1: how are you" 
+echo "$PERSON2: iam good $PERSON1"
+echo "$PERSON1: how are you doing today"
+echo "$PERSON2 : fine thanks $PERSON1" 
