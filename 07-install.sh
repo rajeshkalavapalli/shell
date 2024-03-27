3b1013551d8e661beae56b7cbdd0aebe8c3f7ba2
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-27 19:48:48 +0530

    data types

--- 07-install.sh
+++ 07-install.sh
@@ -2,13 +2,13 @@
 
 ID=$(id -u)
 
-if [  $ID  -eq 0 ] 
+if [  $ID  -ne 0 ] 
 then 
-    echo "error ::your a root user"
-    
-else 
     echo "error:: your  not a root user ...."
     echo "please run your script with root user"
+    exit 1
+else 
+    echo "error ::your a root user"
 fi
 
 yum install mysql  -y
