25c1528a2f5482c53937f1484a6ef37ab46ec471
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-27 19:07:54 +0530

    data types

---
+++ 06-condition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter your number"
+read NUMBER
+
+if [ $NUMBER -gt 100 ]
+then
+    echo "given number is  grater then 100"
+else
+    "given number is not grater then 100"
+fi
