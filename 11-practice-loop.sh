a5013a4075fc92e382a4e19abfb64b50e0e916cf
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-28 10:05:36 +0530

    my practice 1

--- 11-practice-loop.sh
+++ 11-practice-loop.sh
@@ -13,18 +13,18 @@ LOGFILE=/tmp/$0-$TIMESTAMP.log
 validate (){
     if [ $1 -ne 0 ]
     then 
-        echo "$R $2 .......failed $N"
+        echo -e "$R $2 .......failed $N"
     else 
-        echo "$G $2........Success  $N"
+        echo -e "$G $2........Success  $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then 
-    echo "$R you're not a root user $N "
+    echo -e "$R you're not a root user $N "
     
 else 
-    echo "$G you're a root user $N"
+    echo -e "$G you're a root user $N"
 fi 
 
 for package in $@
