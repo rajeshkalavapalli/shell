123d95e323f0b651f8623eee5f1ba1fcabbeaf4b
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-28 09:19:59 +0530

    loos in installation 1

--- 10-installpac-loop.sh
+++ 10-installpac-loop.sh
@@ -6,13 +6,34 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+validate(){
+    if [ $1 -ne 0 ]
+    then 
+    echo "$2 field "
+    else 
+    echo "#2 ...sucess"
+    fi
+}
 if [ $ID -ne 0 ]
 then 
     echo -e "$R your not a root user $N"
-    echo -e "$Y please run this script with root user $N"
+    echo -e "$R please run this script with root user $N"
     exit 1
 else 
     echo -e  " $G your a root user $N"
 fi
 
-    echo "all arguments passed: $@"
+    #echo "all arguments passed: $@"
+
+    for package in $@
+    do 
+    yum list installed $package
+
+    if [ $? -ne 0 ]
+    then 
+        yum install $package -y 
+
+        validate $?  "installed of $package"
+    else
+        echo -e  " $Y $package is allready installed $N"
+    done 
