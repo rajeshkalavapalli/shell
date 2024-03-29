9902b535dde2a6a2305851b0c11d49519e85b6c1
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-29 20:24:52 +0530

    diskusage

--- 14-diskusge.sh
+++ 14-diskusge.sh
@@ -1,21 +1,17 @@
-#!/bin/bash 
+#!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 DISK_THRESHOLD=1
 message=""
 
-while IFS= read line 
+while IFS= read line
 do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
-    
-    usage=$(echo "$line" | awk '{print $6}' | cut -d '%' -f1) 
-    partition=$(df -hT | grep -vE 'tmp|File' | awk '{print $1F}')
-    if [ $usage -gt $DISK_THRESHOLD ]
+    partition=$(echo $line | awk '{print $1F}')
+    if [ $usage -ge $DISK_THRESHOLD ]
     then
-        message+="high disk usage on: $partition:$usage\n"
-    fi 
-
-
+        message+="High Disk Usage on $partition: $usage <br>"
+    fi
 done <<< $DISK_USAGE
 
-echo -e "message: $message"
+echo -e "Message: $message"
