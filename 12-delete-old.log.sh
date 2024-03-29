de83a4d0e8c942da57b8c30369e04e492e4ab657
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-29 18:12:40 +0530

    deleting old file

---
+++ 12-delete-old.log.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+R="\e[31m"
+G="\e[31m"
+Y="\e[31m"
+N="\e[31m"
+
+SOURCE_DIR="/tmp/shellscript-logs"
+
+if [ ! -d  $SOURCE_DIR]
+then
+    echo "source directory : $R $SOURCE_DIR does not exitst $N"
+else 
+    echo "source directory :$G $SOURCE_DIR  exitst $N"
+
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*log")
+
+while IFS= read -r line
+do
+    echo "deleting file :$line" 
+    rm-rf $line
+
+done <<< $FILES_TO_DELETE
+
