f3c9ab86f2fb6df4d30f7ec7e90a14d16c060a81
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-29 20:58:48 +0530

    diskusage

--- 14-diskusge.sh
+++ 14-diskusge.sh
@@ -4,14 +4,14 @@ DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 DISK_THRESHOLD=1
 message=""
 
-while IFS= read line
+while IFS= read -r line
 do
-    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
-    partition=$(echo $line | awk '{print $1F}')
-    if [ $usage -ge $DISK_THRESHOLD ]
-    then
-        message+="High Disk Usage on $partition: $usage\n"
+    usage=$(echo "$line" | awk '{print $6}' | cut -d '%' -f1)  
+    partition=$(echo "$line" | awk '{print $1}')  
+    if [ "$usage" -ge "$DISK_THRESHOLD" ]; then
+        message+="High Disk Usage on $partition: $usage%\n"
     fi
-done <<< $DISK_USAGE
+done <<< "$DISK_USAGE"
 
-echo -e "Message: $message" | mail -s "high disk usage"  lavanya.kalavapalli748@gmail.com
+echo -e "Message:\n$message" | mail -s "High Disk Usage" lavanya.kalavapalli748@gmail.com
+
